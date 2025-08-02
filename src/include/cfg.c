11431ca3cd8ebdd777f84996783ee74773587bb3
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-08-02 06:17:39 +0300

    feat: impl the functions added in cfg.h

--- src/include/cfg.c
+++ src/include/cfg.c
@@ -5,8 +5,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+void cfg_print_token(cfg_Token *token) {
+    printf("type: %s", cfg_get_token_type(token->type));
+    printf(" string_value: '%s'", token->string_value);
+    printf(" line: %d", token->line);
+    printf(" column: %d", token->column);
+    printf("\n");
+}
+
+char *cfg_get_token_type(cfg_TokenType type) {
+    switch (type) {
+    case IDENTIFIRE:
+        return "IDENTIFIRE";
+    case STRING:
+        return "STRING";
+    case NUMBER:
+        return "NUMBER";
+    case BOOLEAN:
+        return "BOOLEAN";
+    case EQUALS:
+        return "EQUALS";
+    case COMMENT:
+        return "COMMENT";
+    case INLINE_COMMENT:
+        return "INLINE_COMMENT";
+    case NEWLINE:
+        return "NEWLINE";
+    case EOF:
+        return "EOF";
+    default:
+        return NULL;
+    }
+}
+
 cfg_Tokens *cfg_lexer(char *text) {
     cfg_Tokens *tokens = malloc(sizeof(cfg_Tokens));
+    tokens->list = malloc(sizeof(cfg_Token) * 128);
+    tokens->last_index = 0;
 
     size_t cursor = 0;
     int line = 1;
@@ -27,10 +62,10 @@ cfg_Tokens *cfg_lexer(char *text) {
         // EOF,
 
         switch (text[cursor]) {
-        case '\n':
+        case '\n': {
             line += 1;
             column = 0;
-            break;
+        } break;
 
         default:
             break;
